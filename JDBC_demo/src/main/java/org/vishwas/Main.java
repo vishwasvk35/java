1d122b8562b319bfa017ed2357a5e8ffe392fdbf
Vishwas Kulkarni <vishwasvk35@gmail.com>
2026-04-09 15:19:44 +0530

    hibernate-init

--- JDBC_demo/src/main/java/org/vishwas/Main.java
+++ JDBC_demo/src/main/java/org/vishwas/Main.java
@@ -2,6 +2,7 @@ package org.vishwas;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
@@ -9,16 +10,22 @@ public class Main {
         Users u1 = new Users();
         u1.setAge(20);
         u1.setName("vishwas");
-        u1.setuID(1);
+//        u1.setuID(1);
 
         System.out.println(u1.toString());
 
         Configuration cnf = new Configuration();
-        cnf.addAnnotatedClass(org.vishwas.Users.class);
+        cnf.addAnnotatedClass(org.vishwas.Users.class); //declare class that acts as schema of database table
+        cnf.configure();
 
         SessionFactory sf = cnf.buildSessionFactory();
         Session session = sf.openSession();
 
-        session.save(u1);
+        Transaction transaction = session.beginTransaction();
+        session.persist(u1);
+        transaction.commit();
+
+        session.close();
+        sf.close();
     }
 }
